adbb811063c216c1dd833bdc648672a53d35001f
avyarkov <jarkov.alexej@gmail.com>
2022-11-06 18:50:55 +0300

    Finished GUI

--- src/main/java/com/github/avyarkov/WindowFrame.java
+++ src/main/java/com/github/avyarkov/WindowFrame.java
@@ -4,15 +4,69 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Map;
 
 class WindowFrame extends JFrame implements ActionListener {
     static final int INF = 1_000_000;
-    static final int topHeight = 50;
+    static final int topHeight = 40;
     static final int gap = 20;
     static final int margin = 20;
 
-    JButton button;
+    static class MyNumberField extends JTextField {
+        static final int maxLength = 2;
+        static final int minValue = 0;
+        static final int maxValue = 99;
+
+        MyNumberField() {
+            super("0");
+            this.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyTyped(KeyEvent ke) {
+                    char inputChar = ke.getKeyChar();
+                    if (!Character.isDigit(inputChar) || getText().length() >= maxLength ) {
+                        ke.consume();
+                    }
+                }
+            });
+        }
+
+        int getNumber() {
+            if (getText().isEmpty()) {
+                setText("0");
+            }
+            return Integer.parseInt(getText());
+        }
+
+        void setNumber(int number) {
+            if (minValue <= number && number <= maxValue) {
+                this.setText(Integer.toString(number));
+            }
+        }
+    }
+
+    static class MyIncrementButtonListener implements ActionListener {
+        int increment;
+        MyNumberField controlledNumberField;
+
+        public MyIncrementButtonListener(MyNumberField controlledNumberField, int increment) {
+            super();
+            this.increment = increment;
+            this.controlledNumberField = controlledNumberField;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            controlledNumberField.setNumber(controlledNumberField.getNumber() + increment);
+        }
+    }
+
+    JComboBox<String> comboBox;
+    MyNumberField inputNumberField;
+    JButton minusButton;
+    JButton plusButton;
+    JButton transposeButton;
     JTextArea input;
     JScrollPane inputScroll;
     JTextArea output;
@@ -35,30 +89,51 @@ class WindowFrame extends JFrame implements ActionListener {
         top.setPreferredSize(new Dimension(INF, topHeight));
         top.setOpaque(false);
 
-        button = new JButton("Transpose");
-        button.setFocusable(false);
-        button.addActionListener(this);
+        JLabel label1 = new JLabel("Transpose");
+        JLabel label2 = new JLabel("by");
+        JLabel label3 = new JLabel("semitones    ");
 
-        top.add(button);
+        String[] options = {"up", "down"};
+        comboBox = new JComboBox<>(options);
+        comboBox.setFocusable(false);
+
+        inputNumberField = new MyNumberField();
+        inputNumberField.setPreferredSize(new Dimension(40, 25));
+
+        minusButton = new JButton("-");
+        minusButton.setFocusable(false);
+        minusButton.addActionListener(new MyIncrementButtonListener(inputNumberField, -1));
+        plusButton = new JButton("+");
+        plusButton.setFocusable(false);
+        plusButton.addActionListener(new MyIncrementButtonListener(inputNumberField, 1));
+
+        transposeButton = new JButton("Transpose");
+        transposeButton.setFocusable(false);
+        transposeButton.addActionListener(this);
+
+        top.add(label1);
+        top.add(comboBox);
+        top.add(label2);
+        top.add(minusButton);
+        top.add(inputNumberField);
+        top.add(plusButton);
+        top.add(label3);
+        top.add(transposeButton);
 
         JPanel center = new JPanel();
         center.setLayout(new GridLayout(1, 2, gap, gap));
         center.setOpaque(false);
 
         input = new JTextArea("Input");
-//        input.setLineWrap(true);
         inputScroll = new JScrollPane(input,
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
             JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-//        inputScroll.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
         output = new JTextArea("Output");
         output.setEditable(false);
-//        output.setLineWrap(true);
         outputScroll = new JScrollPane(output,
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
             JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-//        outputScroll.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
         center.add(inputScroll);
         center.add(outputScroll);
@@ -83,7 +158,7 @@ class WindowFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if (source == button) {
+        if (source == transposeButton) {
             String text = input.getText();
             // TODO
             output.setText(TextParser.replaceWords(text, Map.of("Em", "??")));
